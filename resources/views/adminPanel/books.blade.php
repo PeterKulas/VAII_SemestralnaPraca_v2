3d127e97f43e85c5ffebb627930b0509f9efe788
peterkulas <peterkulass@gmail.com>
2021-11-29 20:59:30 +0100

    created books migration

--- resources/views/adminPanel/books.blade.php
+++ resources/views/adminPanel/books.blade.php
@@ -37,7 +37,8 @@
             </div>
         </div>
         <div class="card">
-            <img class="card-img-top" src="../../images/homer.jpg" alt="Card image cap">
+            <img class="card-img-top" src="https://images.gr-assets.com/books/1358273780m/6148028.jpg"
+                alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
