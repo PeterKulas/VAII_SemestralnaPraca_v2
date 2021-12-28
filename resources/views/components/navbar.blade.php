e124a981d0954649f8b3ec981165a7d2b8185017
peterkulas <peterkulass@gmail.com>
2021-12-28 19:02:22 +0100

    reworked navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,14 +7,15 @@
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="bi bi-justify"></i>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="collapse navbar-collapse" id="navbarNav" style="padding-top: 0px; padding-bottom: 0px;">
                     <ul class="navbar-nav">
                         <li class="nav-item"><a href="/">Domov</a></li>
                         <li class="nav-item"><a href="/about">O nás</a></li>
                         <li class="nav-item"><a href="/contact">Kontakt</a></li>
                         <li>
-                            <div class="login ">
-                                <button type="button" data-bs-toggle="modal" data-bs-target="#loginModal">Prihlásiť
+                            <div class="login">
+                                <button type="button" class="btn btn-outline-light" data-bs-toggle="modal"
+                                    data-bs-target="#loginModal">Prihlásiť
                                     sa</button>
                                 <span><a href="registrationPage">Nemáš vytvorený učet? Registruj sa!</a></span>
                             </div>
