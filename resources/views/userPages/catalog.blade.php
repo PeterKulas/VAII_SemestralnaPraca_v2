1d7915e40918dfc2d09e1232fab8c3e493dab3c1
peterkulas <peterkulass@gmail.com>
2022-01-12 00:19:17 +0100

    added book catalog to user interface

---
+++ resources/views/userPages/catalog.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+
+@section('title', 'Katalog')
+
+@section('content')
+<div class="card-deck row g-0">
+    @foreach($books as $book)
+    <div class="card">
+        <img class="card-img-top img-catalog" src="{{ $book->img }}" alt="{{ $book->title }}">
+        <div class="card-body">
+            <h5 class="card-title">{{ $book->title }}</h5>
+            <hr>
+            <div class="cfooter">
+                <p class="author">{{ $book->authorID }}</p>
+                <a href="#"><i class="bi bi-heart"></i></a>
+            </div>
+
+        </div>
+    </div>
+    @endforeach
+</div>
+@endsection
