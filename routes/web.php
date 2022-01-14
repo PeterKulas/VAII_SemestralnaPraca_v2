4d84b4f205313227cafa15c50745b0c384eab610
peterkulas <peterkulass@gmail.com>
2022-01-14 12:00:18 +0100

    added admin middleware

--- routes/web.php
+++ routes/web.php
@@ -36,41 +36,41 @@ Route::get('register', [RegisterController::class, 'getView'])->middleware('gues
 Route::post('register', [RegisterController::class, 'storeUser'])->middleware('guest');
 
 /*Admin panel */
-Route::get('adminPanel/users', [AdminPanelController::class, 'getUsers']);
-Route::get('adminPanel/books', [AdminPanelController::class, 'getBooks']);
-Route::get('adminPanel/authors', [AdminPanelController::class, 'getAuthors']);
-Route::get('adminPanel/publishers', [AdminPanelController::class, 'getPublishers']);
-Route::get('adminPanel/missingBooks', [MissingBookController::class, 'getMissingBooks']);
+Route::get('adminPanel/users', [AdminPanelController::class, 'getUsers'])->middleware('admin');
+Route::get('adminPanel/books', [AdminPanelController::class, 'getBooks'])->middleware('admin');
+Route::get('adminPanel/authors', [AdminPanelController::class, 'getAuthors'])->middleware('admin');
+Route::get('adminPanel/publishers', [AdminPanelController::class, 'getPublishers'])->middleware('admin');
+Route::get('adminPanel/missingBooks', [MissingBookController::class, 'getMissingBooks'])->middleware('admin');
 
 
-Route::get('adminPanel/users/delete/{id}', [AdminPanelController::class, 'deleteUser']);
-Route::get('adminPanel/users/edit/{id}', [AdminPanelController::class, 'getUser']);
-Route::post('adminPanel/user/edit', [AdminPanelController::class, 'editUser']);
+Route::get('adminPanel/users/delete/{id}', [AdminPanelController::class, 'deleteUser'])->middleware('admin');
+Route::get('adminPanel/users/edit/{id}', [AdminPanelController::class, 'getUser'])->middleware('admin');
+Route::post('adminPanel/user/edit', [AdminPanelController::class, 'editUser'])->middleware('admin');
 
-Route::get('adminPanel/authors/delete/{id}', [AdminPanelController::class, 'deleteAuthor']);
-Route::get('adminPanel/authors/edit/{id}', [AdminPanelController::class, 'getAuthor']);
-Route::post('adminPanel/author/edit', [AdminPanelController::class, 'editAuthor']);
+Route::get('adminPanel/authors/delete/{id}', [AdminPanelController::class, 'deleteAuthor'])->middleware('admin');
+Route::get('adminPanel/authors/edit/{id}', [AdminPanelController::class, 'getAuthor'])->middleware('admin');
+Route::post('adminPanel/author/edit', [AdminPanelController::class, 'editAuthor'])->middleware('admin');
 
-Route::get('adminPanel/books/delete/{id}', [AdminPanelController::class, 'deleteBook']);
-Route::get('adminPanel/books/edit/{id}', [AdminPanelController::class, 'getBook']);
-Route::post('adminPanel/book/edit', [AdminPanelController::class, 'editBook']);
+Route::get('adminPanel/books/delete/{id}', [AdminPanelController::class, 'deleteBook'])->middleware('admin');
+Route::get('adminPanel/books/edit/{id}', [AdminPanelController::class, 'getBook'])->middleware('admin');
+Route::post('adminPanel/book/edit', [AdminPanelController::class, 'editBook'])->middleware('admin');
 
-Route::get('adminPanel/publishers/delete/{id}', [AdminPanelController::class, 'deletePublisher']);
-Route::get('adminPanel/publishers/edit/{id}', [AdminPanelController::class, 'getPublisher']);
-Route::post('adminPanel/publisher/edit', [AdminPanelController::class, 'editPublisher']);
+Route::get('adminPanel/publishers/delete/{id}', [AdminPanelController::class, 'deletePublisher'])->middleware('admin');
+Route::get('adminPanel/publishers/edit/{id}', [AdminPanelController::class, 'getPublisher'])->middleware('admin');
+Route::post('adminPanel/publisher/edit', [AdminPanelController::class, 'editPublisher'])->middleware('admin');
 
-Route::get('adminPanel/missingBooks/delete/{id}', [MissingBookController::class, 'deleteMissingBook']);
+Route::get('adminPanel/missingBooks/delete/{id}', [MissingBookController::class, 'deleteMissingBook'])->middleware('admin');
 
 /*Inserts */
 
-Route::get('adminPanel/authors/insert', [AdminPanelController::class, 'getInsertAuthorView']);
-Route::post('adminPanel/authors/insert', [AdminPanelController::class, 'storeAuthor']);
+Route::get('adminPanel/authors/insert', [AdminPanelController::class, 'getInsertAuthorView'])->middleware('admin');
+Route::post('adminPanel/authors/insert', [AdminPanelController::class, 'storeAuthor'])->middleware('admin');
 
-Route::get('adminPanel/books/insert', [AdminPanelController::class, 'getInsertBookView']);
-Route::post('adminPanel/books/insert', [AdminPanelController::class, 'storeBook']);
+Route::get('adminPanel/books/insert', [AdminPanelController::class, 'getInsertBookView'])->middleware('admin');
+Route::post('adminPanel/books/insert', [AdminPanelController::class, 'storeBook'])->middleware('admin');
 
-Route::get('adminPanel/publishers/insert', [AdminPanelController::class, 'getInsertPublisherView']);
-Route::post('adminPanel/publishers/insert', [AdminPanelController::class, 'storePublisher']);
+Route::get('adminPanel/publishers/insert', [AdminPanelController::class, 'getInsertPublisherView'])->middleware('admin');
+Route::post('adminPanel/publishers/insert', [AdminPanelController::class, 'storePublisher'])->middleware('admin');
 
 /*Login, Logout sessions */
 Route::get('login', [SessionsController::class, 'getLoginView'])->middleware('guest');
@@ -79,12 +79,12 @@ Route::post('login', [SessionsController::class, 'loginUser'])->middleware('gues
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 /*Catalog */
-Route::get('catalog', [CatalogController::class, 'getCatalogView']);
-Route::post('catalog', [CatalogController::class, 'getCatalogSelected']);
+Route::get('catalog', [CatalogController::class, 'getCatalogView'])->middleware('auth');
+Route::post('catalog', [CatalogController::class, 'getCatalogSelected'])->middleware('auth');
 
-Route::get('catalog/singleBook/{id}', [CatalogController::class, 'getSingleBookView']);
-Route::post('catalog/singleBook/{id}', [CatalogController::class, 'ajaxInsertRecension']);
+Route::get('catalog/singleBook/{id}', [CatalogController::class, 'getSingleBookView'])->middleware('auth');
+Route::post('catalog/singleBook/{id}', [CatalogController::class, 'ajaxInsertRecension'])->middleware('auth');
 
 /*Missing page */
-Route::get('missingbook', [MissingBookController::class, 'getView']);
-Route::post('missingbook', [MissingBookController::class, 'storeBook']);
+Route::get('missingbook', [MissingBookController::class, 'getView'])->middleware('auth');
+Route::post('missingbook', [MissingBookController::class, 'storeBook'])->middleware('auth');
